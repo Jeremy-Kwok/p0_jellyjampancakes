acdc5b00f94a116616588e1c6b7fb90d2d082698
Jeremy-Kwok <Jeremykwok917@gmail.com>
2022-11-14 17:19:26 -0500

    11/14

--- app/__init__.py
+++ app/__init__.py
@@ -35,6 +35,11 @@ def index():
 # register
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+
+    input_username = ""
+    input_password = ""
+    input_confirm_password = ""
+
     # breakdown into GET and POST methods
 
     #GET
@@ -43,121 +48,86 @@ def register():
         input_password = request.args['password']
         input_confirm_password = request.args['confirm password']
 
-        #Checks for password/confirm password match
-        if input_password != input_confirm_password:
-            return render_template('register.html', message = "Passwords do not match. Please try again.")
-
-        username_check = f"select username from accounts where username='{input_username}';"
-
-        c.execute(username_check)
-        # if there isn't an account associated with said username then create one
-        if not c.fetchone():
-            c.execute("insert into accounts values(?, ?)", (input_username, input_password))
-            return redirect(url_for('index'))
-        # if username is already taken
-        return render_template('register.html', message = "Username is already taken. Please select another username.")
-
     #POST
     if request.method == 'POST':
         input_username = request.form['username']
         input_password = request.form['password']
         input_confirm_password = request.form['confirm password']
 
-        #Checks for password/confirm password match
-        if input_password != input_confirm_password:
-            return render_template('register.html', message = "Passwords do not match. Please try again.")
+    #Checks for password/confirm password match
+    if input_password != input_confirm_password:
+        return render_template('register.html', message = "Passwords do not match. Please try again.")
 
-        username_check = f"select username from accounts where username='{input_username}';"
-
-        c.execute(username_check)
-        # if there isn't an account associated with said username then create one
-        if not c.fetchone():
-            c.execute("insert into accounts values(?, ?)", (input_username, input_password))
-        # if username is already taken
+    #Checks for existing username in accounts table
+    c.execute(f"select username from accounts where username='{input_username}';")
+    
+    # if there isn't an account associated with said username then create one
+    if not c.fetchone():
+        c.execute("insert into accounts values(?, ?)", (input_username, input_password))
+        return render_template('login.html')
+    # if username is already taken
+    else:
         return render_template('register.html', message = "Username is already taken. Please select another username.")
 
 # redirect to user registration page
-@app.route("/user_registration", methods=['GET', 'POST'])
+@app.route("/redirect_register", methods=['GET', 'POST'])
 def redirect_register():
     return render_template('register.html')
 
 # LOGIN
 
 # if user doesn't already have a session then prompt login
-@app.route("/login", methods=['GET', 'POST'])
+@app.route("/redirect_login", methods=['GET', 'POST'])
 def redirect_login():
     print(session)
     return render_template('login.html')
 
-# authorize
-@app.route("/auth", methods=['GET', 'POST'])
-def authenticate():
-    # checks to see if the user enters a valid login, and creates a new session if so
+# login process
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+
+    # GET
     if request.method == 'GET':
         input_username = request.args['username']
         input_password = request.args['password']
 
-        # Searchs accounts table for user-password combination
-        login_check = f"select username from accounts where username='{input_username}' and password = '{input_password}';"
-        username_check = f"select username from accounts where username='{input_username}';"
-        password_check = f"select username from accounts where password='{input_password}';"
-
-        c.execute(login_check)
-        if c.fetchone():
-            print("Login success!")
-            session['username'] = request.args['username'] # stores username in session
-            return render_template('feed.html', username = request.args['username'], password = request.args['password'], method = request.method)  #For 'get'
-
-        else:
-            print("Login failed")
-            error_msg = ''
-            
-            # Username check
-            c.execute(username_check)
-            if not c.fetchone():
-                error_msg += "Username is incorrect or not found. \n"
-            
-            #Password check
-            c.execute(password_check)
-            if not c.fetchone():
-                error_msg += "Password is incorrect or not found. \n"
-
-            error_msg += "Please try again."
-            return render_template('login.html', message = error_msg)
-
-
-    # same thing, just adjusted for POST requests
+    # POST
     if request.method == 'POST':
         input_username = request.form['username']
         input_password = request.form['password']
 
-        # Searchs accounts table for user-password combination
-        login_check = f"select username from accounts where username='{input_username}' and password = '{input_password}';"
-        username_check = f"select username from accounts where username='{input_username}';"
-        password_check = f"select username from accounts where password='{input_password}';"
+    # Searchs accounts table for user-password combination
+    login_check = f"select username from accounts where username='{input_username}' and password = '{input_password}';"
+    username_check = f"select username from accounts where username='{input_username}';"
+    password_check = f"select username from accounts where password='{input_password}';"
 
-        c.execute(login_check)
-        if c.fetchone():
-            print("Login success!")
-            session['username'] = request.form['username'] # stores username in session
-            return render_template('feed.html', username = request.form['username'], password = request.form['password'], method = request.method)  #For 'post'
+    c.execute(login_check)
+    if c.fetchone():
+        print("Login success!")
+        if request.method == 'GET': #For 'get'
+            session['username'] = request.args['username'] # stores username in session 
 
-        else:
-            print("Login failed")
-            error_msg = ''
+        if request.method == 'POST': #For 'post'
+            session['username'] = request.form['username'] # stores username in session 
+        
+        return render_template('feed.html', username = session['username'])
+
+    else:
+        print("Login failed")
+        error_msg = ''
             
-            # Username check
-            c.execute(username_check)
-            if not c.fetchone():
-                error_msg += "Username is incorrect. \n"
-            
-            #Password check
-            c.execute(password_check)
-            if not c.fetchone():
-                error_msg += "Password is incorrect. \n"
+        # Username check
+        c.execute(username_check)
+        if not c.fetchone():
+            error_msg += "Username is incorrect or not found. \n"
+        
+        #Password check
+        c.execute(password_check)
+        if not c.fetchone():
+            error_msg += "Password is incorrect or not found. \n"
 
-            error_msg += "Please try again."
-            return render_template('login.html', message = error_msg)
+        error_msg += "Please try again."
+        return render_template('login.html', message = error_msg)
 
 # logout and redirect to login page
 @app.route("/logout", methods=['GET', 'POST'])
@@ -175,52 +145,35 @@ def redirect_create():
 def create():
 
     username = session['username']
+    date = datetime.datetime.now().strftime("%y-%m-%d")
+    time = datetime.datetime.now().strftime("%H:%M:%S")
     
     #GET
     if request.method == 'GET':
         #storyID = 
         storyTitle = request.args['storyTitle']
         storyContent = request.args['storyContent']
-        date = datetime.datetime.now().strftime("%y-%m-%d")
-        time = datetime.datetime.now().strftime("%H:%M:%S")
-
-        print("Story Title: " + storyTitle)
-        print("Story Content: " + storyContent)
-
-        title_check = f"select storyTitle from stories where storyTitle='{storyTitle}';"
-
-        c.execute(title_check)
-        # if there isn't an story associated with said title then create one
-        if not c.fetchone():
-            c.execute("insert into stories values(?, ?, ?, ?, ?)", (storyTitle, storyContent, username, date, time))
-            print("Publish Success!")
-            return redirect(url_for('redirect_feed'))
-        # if storyTitle is already taken
-        print("Publish fail: Title already taken")
-        return render_template('create.html', message = "Story Title is already taken. Please select another Title.")
 
     #POST
     if request.method == 'POST':
         #storyID = 
         storyTitle = request.form['storyTitle']
         storyContent = request.form['storyContent']
-        date = datetime.datetime.now().strftime("%y-%m-%d")
-        time = datetime.datetime.now().strftime("%H:%M:%S")
 
-        print("Story Title: " + storyTitle)
-        print("Story Content: " + storyContent)
+    print("Story Title: " + storyTitle)
+    print("Story Content: " + storyContent)
 
-        title_check = f"select storyTitle from stories where storyTitle='{storyTitle}';"
+    c.execute(f"select storyTitle from stories where storyTitle='{storyTitle}';")
 
-        c.execute(title_check)
-        # if there isn't an story associated with said title then create one
-        if not c.fetchone():
-            c.execute("insert into stories values(?, ?, ?, ?, ?)", (storyTitle, storyContent, username, date, time))
-            print("Publish Success!")
-            return redirect(url_for('redirect_feed'))
-        # if storyTitle is already taken
-        print("Publish fail: Title already taken")
-        return render_template('create.html', message = "Story Title is already taken. Please select another Title.")
+    # if there isn't an story associated with said title then create one
+    if not c.fetchone():
+        c.execute("insert into stories values(?, ?, ?, ?, ?)", (storyTitle, storyContent, username, date, time))
+        print("Publish Success!")
+        return redirect(url_for('redirect_feed'))
+
+    # if storyTitle is already taken
+    print("Publish fail: Title already taken")
+    return render_template('create.html', message = "Story Title is already taken. Please select another Title.")
 
 @app.route("/redirect_feed", methods=['GET', 'POST'])
 def redirect_feed():
