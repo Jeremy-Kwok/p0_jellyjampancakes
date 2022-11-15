32383856775e49f1209f37afafae881ea4130f79
Jeremy-Kwok <Jeremykwok917@gmail.com>
2022-11-15 01:40:05 -0500

    11/14 THE LIBRARY WORKS

--- README.md
+++ README.md
@@ -30,6 +30,18 @@ A line such as below will appear in your terminal. Copy and paste the given URL 
 
     check_same_thread: https://www.folkstalk.com/2022/09/sqlite3-programmingerror-sqlite-objects-created-in-a-thread-can-only-be-used-in-that-same-thread-with-code-examples.html
 
+    SQLite3:
+        -Tables can be sorted in an ASC and DESC order:
+        https://www.sqlitetutorial.net/sqlite-order-by/
+
+        -To get only n row(s) from a select command, use limit n
+        -Insert into [tableName] select... allows you to insert a row from another table into [tableName]
+
+        -To update an entire column at once use "update [tableName] set [columnName]='_____'"
+        https://www.sqlitetutorial.net/sqlite-update/ 
+
+        -"select distinct [columnName]" allows you to avoid selecting rows with duplicate values in a [columnName]
+
     Python-Flask:
         -redirect(url_for('[page path]')) redirects user to that page
 
@@ -43,6 +55,7 @@ A line such as below will appear in your terminal. Copy and paste the given URL 
     Why does the website sometimes render "Access to 127.0.0.1 was denied" when we run it? But then it goes on to work in an incognito window.
 
     
+
 
 
 #
--- app/__init__.py
+++ app/__init__.py
@@ -183,11 +183,101 @@ def redirect_create():
 @app.route("/library", methods=['GET', 'POST'])
 def library():
 
-    return 
+    username = session['username']
+
+    # deletes old recentFeed table and creates a new one
+    c.execute("drop table recentFeed;")
+    c.execute("create table if not exists recentFeed(storyTitle text, storyContent text, username text, date text, time text, userEdit int);")
+
+    # FOR STORIES THE USER HAS ALREADY EDITED
+
+    # takes all unique titles in stories table that the username has edited
+    c.execute(f"select distinct storyTitle from stories where username='{username}';")
+    editedTitles = c.fetchall()
+
+    # for each storyTitle:
+    # 1. sorts entries with most recent at the top, 
+    # 2. selects the most recently edited story record 
+    # 3. inputs it into recentFeed
+    for rows in editedTitles:
+        c.execute(f"insert into recentFeed select * from stories where storyTitle='{rows}' order by date DESC, time DESC limit 1;")
+
+    # for all added entries, input 1 to indicate the user has edited them already
+    c.execute("update recentFeed set userEdited=1")
+    
+    # FOR STORIES THE USER HAS NOT EDITED
+
+    # takes all unique titles in stories table that the username has NOT edited
+    c.execute(f"select distinct storyTitle from stories where username!='{username}';")
+    uneditedTitles = c.fetchall()
+
+    # for each storyTitle:
+    # 1. sorts all entries with most recent at the top, 
+    # 2. selects the most recently edited story record 
+    # 3. inputs it into recentFeed
+    for rows in editedTitles:
+        c.execute(f"insert into recentFeed select * from stories where storyTitle='{rows}' order by date DESC, time DESC limit 1;")
+
+    # for all newly added entries, input 0 to indicate the user has NOT edited them yet
+    c.execute("update recentFeed set userEdited=0 where userEdited!=1")
+
+    # send everything in recentEdited to library.html
+    c.execute("select * from recentFeed")
+    results = c.fetchall()
+    return render_template("library.html", feed = results)
+
 
 @app.route("/redirect_library", methods=['GET', 'POST'])
 def redirect_library():
-    return render_template('library.html', message = "")
+    
+    username = session['username']
+
+    # deletes old recentFeed table and creates a new one
+    c.execute("drop table recentFeed;")
+    c.execute("create table if not exists recentFeed(storyTitle text, storyContent text, username text, date text, time text, userEdited int);")
+
+    # FOR STORIES THE USER HAS ALREADY EDITED
+
+    # takes all unique titles in stories table that the username has edited
+    c.execute(f"select distinct storyTitle from stories where username='{username}';")
+    editedTitles = c.fetchall()
+    print(editedTitles)
+
+    # for each storyTitle:
+    # 1. sorts entries with most recent at the top, 
+    # 2. selects the most recently edited story record 
+    # 3. inputs it into recentFeed
+    for tupleTitles in editedTitles:
+        titles = tupleTitles[0]
+        print("Edited: " + titles)
+        c.execute(f"insert into recentFeed (storyTitle, storyContent, username, date, time) select storyTitle, storyContent, username, date, time from stories where storyTitle='{titles}' order by date DESC, time DESC limit 1;")
+
+    # for all added entries, input 1 to indicate the user has edited them already
+    c.execute("update recentFeed set userEdited=1")
+    
+    # FOR STORIES THE USER HAS NOT EDITED
+
+    # takes all unique titles in stories table that the username has NOT edited
+    c.execute(f"select distinct storyTitle from stories where username!='{username}';")
+    uneditedTitles = c.fetchall()
+
+    # for each storyTitle:
+    # 1. sorts all entries with most recent at the top, 
+    # 2. selects the most recently edited story record 
+    # 3. inputs it into recentFeed
+    for tupleTitles in uneditedTitles:
+        titles = tupleTitles[0]
+        print("Unedited: " + titles)
+        c.execute(f"insert into recentFeed select * from stories where storyTitle='{titles}' order by date DESC, time DESC limit 1;")
+
+    # for all newly added entries, input 0 to indicate the user has NOT edited them yet
+    c.execute("update recentFeed set userEdited=0 where userEdited!=1")
+
+    # send everything in recentEdited to library.html
+    c.execute("select * from recentFeed")
+    results = c.fetchall()
+    #print("Results: " + results)
+    return render_template("library.html", feed = results)
 
 @app.route('/view')
 def view():
